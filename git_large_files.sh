648c6141670aa3abc828a8ae2942e69c38ee57e9
GD <gd0818109@163.com>
2025-01-16 13:39:27 +0800

    1

--- git_large_files.sh
+++
@@ -1,7 +0,0 @@
-git rev-list --objects --all |
-while read sha file; do
-    size=$(git cat-file -s $sha)
-    if [ "$size" -gt $((1024 * 1024)) ]; then
-        echo "$((size / 1024 / 1024)) MB $sha $file"
-    fi
-done | sort -n -r | head -n 10
